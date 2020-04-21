b54125ee6515cc9d977dddd1d0e1226b0ac8554c
Gajendra Mehra <Gajendramehra14rgnv@gmail.com>
2020-04-21 16:56:53 +0530

    User schema added

---
+++ projectbackend/models/order.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const {
+    ObjectId
+} = mongoose.Schema;
+
+const productCartSchema = new mongoose.Schema({
+    product: {
+        type: ObjectId,
+        ref: "Product"
+    },
+    name: String,
+    count: Number,
+    price: Number
+})
+
+const OrderSchema = mongoose.Schema({
+    products: [productCartSchema],
+    transaction_id: {
+
+    },
+    amount: {
+        type: Number
+    },
+    address: {
+        type: String
+    },
+    updated: Date,
+    user: {
+        type: ObjectId,
+        ref: "User"
+    }
+
+}, {
+    timestamps: true
+})
+
+const Order = mongoose.model("Order", OrderSchema)
+const ProductCart = mongoose.model("ProductCart", productCartSchema)
+
+module.exports = {
+    Order,
+    ProductCart
+}
