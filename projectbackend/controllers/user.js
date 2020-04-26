8a9345b93be82a82af06000e2d42334062d1134d
Gajendra Mehra <Gajendramehra14rgnv@gmail.com>
2020-04-26 12:40:46 +0530

    Update purchase list added

--- projectbackend/controllers/user.js
+++ projectbackend/controllers/user.js
@@ -1,4 +1,7 @@
 const User = require("../models/user")
+const {
+    Order
+} = require("../models/order")
 
 
 exports.getUserById = (req, res, next, id) => {
@@ -48,4 +51,58 @@ exports.updateUser = (req, res, next) => {
             })
         })
 
+}
+
+exports.userPurchaseList = (req, res) => {
+
+    console.log(Order);
+
+    Order.find({
+        user: req.profile._id
+    }).populate("user", "_id name").exec((err, order) => {
+        if (err || !order) {
+            return res.status(400).json({
+                error: "No order found"
+            })
+        }
+        return res.status(200).json({
+            message: "Order",
+            orders: order
+        })
+    })
+}
+
+exports.updatePurhaseList = (req, res) => {
+    let purchases = [];
+    req.body.order.products.forEach(product => {
+        purchases.push({
+            _id: product.id,
+            name: product.name,
+            description: product.description,
+            category: product.category,
+            quantity: product.quantity,
+            amount: req.body.order.amount,
+            tramsactionId: req.body.order.tramsactionId
+        })
+
+        User.findOneAndUpdate({
+            _id: req.profile._id
+        }, {
+            $push: {
+                purchases: purchases
+            }
+        }, {
+            new: true
+        }, (err, purchase) => {
+            if (err || !purchase) {
+                res.status(400).json({
+                    error: "Unable to save."
+                })
+            }
+            res.status(200).json({
+                purchases: purchase
+            })
+            next()
+        })
+    });
 }
--- projectbackend/routes/user.js
+++ projectbackend/routes/user.js
@@ -4,7 +4,8 @@ const router = express.Router();
 const {
     getUserById,
     getUser,
-    updateUser
+    updateUser,
+    userPurchaseList
 } = require("../controllers/user")
 const {
     isSignedIn,
@@ -16,5 +17,6 @@ router.param("userId", getUserById)
 router.get("/user/:userId", isSignedIn, isAuthenticated, getUser)
 
 router.put("/user/:userId", isSignedIn, isAuthenticated, updateUser)
+router.get("/orders/user/:userId", isSignedIn, isAuthenticated, userPurchaseList)
 
 module.exports = router
